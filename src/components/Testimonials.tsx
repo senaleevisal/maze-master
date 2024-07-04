f582b53a2e923fc308d1285ff315e21f3972b266
Nethsara Prabash <138272058+nethsaraPrabash@users.noreply.github.com>
2024-07-04 15:01:45 +0530

    About stage_01

--- src/components/Testimonials.tsx
+++ src/components/Testimonials.tsx
@@ -11,44 +11,32 @@ export const Testimonials = () => {
     <Container>
       <div className="grid gap-10 lg:grid-cols-2 xl:grid-cols-3">
         <div className="lg:col-span-2 xl:col-auto">
-          <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              Share a real <Mark>testimonial</Mark>
-              that hits some of your benefits from one of your popular customer.
-            </p>
+          <div className="flex flex-col justify-between w-50 h-30 bg-gray-100 rounded-2xl  dark:bg-trueGray-800">
 
             <Avatar
               image={userOneImg}
-              name="Sarah Steiner"
-              title="VP Sales at Google"
+              name="Kalum Tharindu"
+              title="Organizing co-ordinator/Batch Representative"
             />
           </div>
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              Make sure you only pick the <Mark>right sentence</Mark>
-              to keep it short and simple.
-            </p>
 
             <Avatar
               image={userTwoImg}
-              name="Dylan Ambrose"
-              title="Lead marketer at Netflix"
+              name="Sawindu Samuditha Jayasinghe"
+              title="Organizing co-ordinator/Batch Representative"
             />
           </div>
         </div>
         <div className="">
           <div className="flex flex-col justify-between w-full h-full bg-gray-100 px-14 rounded-2xl py-14 dark:bg-trueGray-800">
-            <p className="text-2xl leading-normal ">
-              This is an <Mark>awesome</Mark> landing page template I&apos;ve
-              seen. I would use this for anything.
-            </p>
 
             <Avatar
               image={userThreeImg}
-              name="Gabrielle Winn"
-              title="Co-founder of Acme Inc"
+              name="Navodya Dewmini"
+              title="Organizing co-ordinator"
             />
           </div>
         </div>
@@ -65,12 +53,12 @@ interface AvatarProps {
 
 function Avatar(props: Readonly<AvatarProps>) {
   return (
-    <div className="flex items-center mt-8 space-x-3">
-      <div className="flex-shrink-0 overflow-hidden rounded-full w-14 h-14">
+    <div className="flex items-center  space-x-3">
+      <div className="flex-shrink-0 overflow-hidden rounded-l-lg ">
         <Image
           src={props.image}
-          width="40"
-          height="40"
+          width="120"
+          height="120"
           alt="Avatar"
           placeholder="blur"
         />
