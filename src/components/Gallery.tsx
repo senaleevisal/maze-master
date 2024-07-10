52799d90b6f8d099e3aa4d71cca5aab3bf9fe676
Senalee Visal <m.m.senaleevisal@gmail.com>
2024-07-10 12:46:49 +0530

    final commit

--- src/components/Gallery.tsx
+++ src/components/Gallery.tsx
@@ -1,27 +1,38 @@
-import React from 'react'
+
+import React, { useState } from 'react'
 import Image from 'next/image'
 
 export const Gallery = () => {
+    const [selectedImage, setSelectedImage] = useState<string | null>(null);
+
+    const handleImageClick = (imageSrc: string) => {
+        setSelectedImage(imageSrc);
+    }
+
     return (
         <div className="grid gap-4 p-4 md:p-8 lg:p-12">
             <div>
-                <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_1.jpeg" alt="" width={2000} height={1000}/>
+                {selectedImage ? (
+                    <Image className="h-auto max-w-full rounded-lg" src={selectedImage} alt="" width={2000} height={1000} />
+                ) : (
+                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_1.jpeg" alt="" width={2000} height={1000} />
+                )}
             </div>
             <div className="grid grid-cols-5 gap-4">
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_2.jpeg" alt="" width={200} height={200}/>
+                <div onClick={() => handleImageClick("/img/carosoul_2.jpeg")}>
+                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_2.jpeg" alt="" width={200} height={200} />
                 </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_3.jpeg" alt="" width={200} height={200}/>
+                <div onClick={() => handleImageClick("/img/carosoul_3.jpeg")}>
+                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_3.jpeg" alt="" width={200} height={200} />
                 </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_4.jpeg" alt="" width={200} height={200}/>
+                <div onClick={() => handleImageClick("/img/carosoul_4.jpeg")}>
+                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_4.jpeg" alt="" width={200} height={200} />
                 </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_5.jpeg" alt="" width={200} height={200}/>
+                <div onClick={() => handleImageClick("/img/carosoul_5.jpeg")}>
+                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_5.jpeg" alt="" width={200} height={200} />
                 </div>
-                <div>
-                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_1.jpeg" alt="" width={200} height={200}/>
+                <div onClick={() => handleImageClick("/img/carosoul_1.jpeg")}>
+                    <Image className="h-auto max-w-full rounded-lg" src="/img/carosoul_1.jpeg" alt="" width={200} height={200} />
                 </div>
             </div>
         </div>
